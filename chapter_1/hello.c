6bf5d43d023f221aa6478df67ef798078d3b947f
drobati <derek.robati@gmail.com>
2012-06-05 10:53:16 -0400

    Initial commit.
    Created folder chapter_1/
    Created file chapter_1/hello.c
    Created file chapter_1/fahr_to_celsius.c

---
+++ chapter_1/hello.c
@@ -0,0 +1,12 @@
+// Derek Robati
+// June 5, 2012
+// Exercise 1-1 and 1-2 completed on this file.
+//
+#include <stdio.h>
+
+main()
+{
+	printf("Hello\tworld!\n");
+	printf("I\tam\n");
+	printf("Derek\tRobati.\n");
+}
