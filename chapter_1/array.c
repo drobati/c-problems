80c0948da5fa1ad5871486510e96ed10520e2da7
drobati <derek.robati@gmail.com>
2012-06-08 12:12:18 -0400

    Modified array.c to complete Exercise 1-13.
    This is a vertical histogram of the characters inputed.

--- chapter_1/array.c
+++ chapter_1/array.c
@@ -1,12 +1,20 @@
 // Derek Robati
 // June 8th, 2012
 //
+//
+// Modified file to complete exercise in book.
+// 1-13. Write a program to print a histogram
+// of the lengths of chars in its input.
+// Vertical orientation was suppose to be
+// more "challenging".
 #include <stdio.h>
 
 main()
 {
 	int c, i, nwhite, nother;
 	int ndigit[10];
+
+	int vheight = 0;
 
 	nwhite = nother = 0;
 	for (i = 0; i < 10; ++i)
@@ -20,9 +28,30 @@ main()
 		else
 			++nother;
 
-	printf("digits =");
+	vheight = nother; //saves some time for a future comparison.
 	for (i = 0; i < 10; ++i)
-		printf(" %d", ndigit[i]);
-	printf(", white space = %d, other = %d\n",
-			nwhite, nother);
+		if (ndigit[i] > vheight)
+			vheight = ndigit[i];
+	if (nwhite > vheight)
+		vheight = nwhite;
+	printf("\n\n");
+	while (vheight >= 0) {
+		printf("%d ", vheight);
+		for (i = 0; i < 10; ++i)
+			if (ndigit[i] >= vheight)
+				printf("| ");
+			else
+				printf("  ");
+		if (nwhite >= vheight)
+			printf("| ");
+		else
+			printf("  ");
+		if (nother >= vheight)
+			printf("| ");
+		else
+			printf("  ");
+		--vheight;
+		printf("\n");
+	}
+	printf("  0 1 2 3 4 5 6 7 8 9 W O\n");
 }
