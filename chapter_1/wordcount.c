ec29daef59b1c20a18e99c730858b58c638a6d0c
drobati <derek.robati@gmail.com>
2012-06-08 10:26:53 -0400

    Modified to print inputed words one per a line.

--- chapter_1/wordcount.c
+++ chapter_1/wordcount.c
@@ -9,20 +9,18 @@
 /* count lines, words, and characters in input */
 main()
 {
-	int c, nl, nw, nc, state;
+	int c, state;
 
 	state = OUT;
-	nl = nw = nc = 0;
 	while ((c = getchar()) != EOF) {
-		++nc;
-		if (c == '\n')
-			++nl;
-		if (c == ' ' || c == '\n' || c == '\t')
+		if (c == ' ' || c == '\n' || c == '\t') {
+			printf("\n");
 			state = OUT;
+		}
 		else if (state == OUT) {
 			state = IN;
-			++nw;
 		}
+		if (state == IN)
+			printf("%c", c);
 	}
-	printf("%d %d %d\n", nl, nw, nc);
 }
